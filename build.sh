328f2b0b8bae268b5b13e5e906142ea59bc8de1f
Demon_player <a18165943161@outlook.com>
2024-06-23 21:54:07 +0800

    为build.sh添加debug选项。

--- build.sh
+++ build.sh
@@ -21,8 +21,13 @@ fi
 
 mkdir -p ${home}/opt
 
+#DEBUG
+if false;then
+    debug="-fsanitize=address -fno-omit-frame-pointer -g"
+fi
+
 echo "Will build:${build}"
 for item in ${build};do
     echo "Building:${item}"
-    g++ -Wall -Wextra -pedantic ${home}/${item}.cpp -o ${home}/opt/${item} `pkg-config --cflags --libs opencv4 openssl`
+    g++ ${debug} -Wall -Wextra -pedantic ${home}/${item}.cpp -o ${home}/opt/${item} `pkg-config --cflags --libs opencv4 openssl`
 done
